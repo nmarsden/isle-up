05034ad16621773c636339bc0e6d28fa73ef4526
Neil Marsden <neil.marsden@oracle.com>
2025-04-18 10:47:00 +1000

    - island: remove unused code

--- src/components/island.tsx
+++ src/components/island.tsx
@@ -5,40 +5,6 @@ import {folder, useControls} from "leva";
 import {GlobalState, useGlobalStore} from "../stores/useGlobalStore.ts";
 import {useGLTF} from "@react-three/drei";
 
-/**
- * Displacement
- */
-// 2D canvas
-const displacementCanvas = document.createElement('canvas');
-displacementCanvas.width = 128
-displacementCanvas.height = 128
-displacementCanvas.style.position = 'fixed'
-displacementCanvas.style.width = '256px'
-displacementCanvas.style.height = '256px'
-displacementCanvas.style.top = '0'
-displacementCanvas.style.left = '0'
-displacementCanvas.style.zIndex = '10'
-document.body.append(displacementCanvas)
-
-// Context
-const displacementContext = displacementCanvas.getContext('2d') as CanvasRenderingContext2D;
-displacementContext.fillRect(0, 0, displacementCanvas.width, displacementCanvas.height);
-
-// Mound image
-const displacementMoundImage = new Image()
-displacementMoundImage.src = './textures/Circular.png'
-
-// Texture
-// const displacementTexture = new CanvasTexture(displacementCanvas);
-
-// Mounds
-// const mounds: { x: number; y: number; off: boolean }[] = [];
-// for (let x=0; x<5; x++) {
-//   for (let y=0; y<5; y++) {
-//     mounds.push({ x, y, off: (x === 2 && y === 2) });
-//   }
-// }
-
 const uniforms = {
   uBaseColor: { value: new Color() },
   uGrassColor: { value: new Color() },
@@ -63,7 +29,7 @@ export default function Island() {
   const planeMesh = useRef<Mesh>(null!);
 
   const {
-    underwaterColor, planeColor, planeMetalness, planeRoughness, positionY, planeDisplacementScale, planeSize, planeSegments, planeWireframe, planeFlatShading, planeShadows
+    underwaterColor, planeMetalness, planeRoughness, positionY, planeWireframe, planeFlatShading, planeShadows
   } = useControls(
     'Island',
     {
@@ -73,18 +39,11 @@ export default function Island() {
           grassColor: { value: "#85a02b", label: "Grass", onChange: (value) => { uniforms.uGrassColor.value = new Color(value); } },
           underwaterColor: { value: "#118a4f", label: "Underwater", transient: false, onChange: (value) => { uniforms.uUnderwaterColor.value = new Color(value); } },
           positionY: { value: 0, label: 'positionY', min: -10, max: 10, step: 0.05 },
-          planeColor: {value: '#f8ed3b', label: 'color'},
           planeMetalness: { value: 0.0, label: 'metalness', min: 0, max: 1, step: 0.01 },
           planeRoughness: { value: 0.7, label: 'roughness', min: 0, max: 1, step: 0.01 },
-          planeDisplacementScale: { value: 0.5, label: 'displacementScale', min: 0, max: 10, step: 0.1 },
-          planeSize: { value: 1, label: 'size', min: 1, max: 100, step: 1 },
-          planeSegments: { value: 160, label: 'segments', min: 10, max: 1000, step: 1 },
           planeWireframe: { value: false, label: 'wireframe' },
           planeFlatShading: { value: false, label: 'flatShading' },
           planeShadows: { value: true, label: 'shadows' },
-          planeMoundOffRatio: { value: 0.75, label: 'moundOffRatio', min: 0.01, max: 4.0, step: 0.01 },
-          planeMoundOffAlpha: { value: 0.5, label: 'moundOffAlpha', min: 0, max: 1, step: 0.001 },
-          planeMoundClearAlpha: { value: 1.00, label: 'moundClearAlpha', min: 0, max: 1, step: 0.001 }
         }
       )
     },
@@ -100,16 +59,10 @@ export default function Island() {
 
   const { planeMaterial } = useMemo(() => {
 
-    // Plane Geometry
-    // const planeGeometry = new PlaneGeometry(planeSize, planeSize, planeSegments, planeSegments);
-
     const planeMaterial = new MeshStandardMaterial({
-      color: planeColor,
       wireframe: planeWireframe,
       roughness: planeRoughness,
       metalness: planeMetalness,
-      // displacementMap: displacementTexture,
-      // displacementScale: planeDisplacementScale,
       flatShading: planeFlatShading,
       toneMapped: false
     });
@@ -204,56 +157,10 @@ export default function Island() {
     }
 
     return { planeMaterial };
-
   },
   [
-    planeColor, planeMetalness, planeRoughness, planeDisplacementScale, planeSize, planeSegments, planeWireframe, planeFlatShading
+    planeMetalness, planeRoughness, planeWireframe, planeFlatShading
   ]);
-
-  useFrame(() => {
-    /**
-     * Displacement
-     */
-    // displacementContext.globalCompositeOperation = 'source-over';
-    // displacementContext.globalAlpha = planeMoundClearAlpha;
-    // displacementContext.fillRect(0, 0, displacementCanvas.width, displacementCanvas.height)
-    //
-    // // Draw mounds
-    // displacementContext.globalAlpha = 1;
-    // displacementContext.drawImage(
-    //   displacementMoundImage,
-    //   0,
-    //   0,
-    //   displacementCanvas.width,
-    //   displacementCanvas.width
-    // )
-
-    // const numCells = 5;
-    // const cellSize = displacementCanvas.width / numCells;
-    // const moundOnSize = cellSize;
-    // const moundOnOffset = (cellSize - moundOnSize) * 0.5;
-    //
-    // const moundOffSize = cellSize * planeMoundOffRatio;
-    // const moundOffOffset = (cellSize - moundOffSize) * 0.5;
-    // displacementContext.globalCompositeOperation = 'lighten';
-    // mounds.forEach(mound => {
-    //   const offset = mound.off ? moundOffOffset : moundOnOffset;
-    //   const x = ((mound.x) * cellSize) + offset;
-    //   const y = ((mound.y) * cellSize) + offset;
-    //   const size = mound.off ? moundOffSize : moundOnSize
-    //   displacementContext.globalAlpha = mound.off ? planeMoundOffAlpha : 1;
-    //   displacementContext.drawImage(
-    //     displacementMoundImage,
-    //     x,
-    //     y,
-    //     size,
-    //     size
-    //   )
-    // });
-
-    // Texture
-    // displacementTexture.needsUpdate = true
-  });
 
   useFrame(({ clock }) => {
     uniforms.uTime.value = clock.getElapsedTime();
@@ -263,7 +170,6 @@ export default function Island() {
     <mesh
       ref={planeMesh}
       position-y={positionY}
-      // rotation-x = {Math.PI * -0.5}
       castShadow={planeShadows}
       receiveShadow={planeShadows}
       geometry={planeGeometry}
@@ -271,7 +177,8 @@ export default function Island() {
     />
     <mesh
       rotation-x={-Math.PI / 2}
-      position={[0, (positionY - 0.01), 0]} // Moved it down to prevent the visual glitch from plane collision
+      position={[0, -0.01, 0]} // Moved it down to prevent the visual glitch from plane collision
+      // position={[0, (positionY - 0.01), 0]} // Moved it down to prevent the visual glitch from plane collision
       receiveShadow={true}
     >
       <planeGeometry args={[256, 256]} />
