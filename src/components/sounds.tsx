ed34b086e14eb3caaa6629f7f463d3f85f2c32c6
Neil Marsden <neil.marsden@oracle.com>
2025-05-02 23:10:02 +1000

    vary the wave sounds

--- src/components/sounds.tsx
+++ src/components/sounds.tsx
@@ -2,7 +2,10 @@ import { Howl, Howler } from "howler";
 import { useEffect } from "react";
 import { GlobalState, useGlobalStore } from "../stores/useGlobalStore";
 
-const SOUND_WAVE = new Howl({ src: ['audio/wave.webm', 'audio/wave.mp3'], format: ['webm', 'mp3']});
+const SOUND_WAVES = [
+  new Howl({ src: ['audio/wave.webm', 'audio/wave.mp3'], format: ['webm', 'mp3']}),
+  new Howl({ src: ['audio/wave2.webm', 'audio/wave2.mp3'], format: ['webm', 'mp3']})
+];
 
 export default function Sound() {
   const playing = useGlobalStore((state: GlobalState) => state.playing);
@@ -16,7 +19,15 @@ export default function Sound() {
   useEffect(() => {
     if (!playing) return;
 
-    SOUND_WAVE.play();
+    const soundIndex = Math.floor(Math.random() * 2);
+    const rate = 0.3 + Math.random() * 0.3;
+    const volume = 0.3 + (soundIndex * 0.2) + Math.random() * 0.2;
+    const duration = (300 + Math.random() * 200);
+    const soundWave = SOUND_WAVES[soundIndex];
+
+    const id = soundWave.play();
+    soundWave.rate(rate, id);
+    soundWave.fade(volume, 0, duration, id);
 
   }, [ playing, moves ])
 
